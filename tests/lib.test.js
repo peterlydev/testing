f2b8de91fd3e9c20cd3eca7e3a225cad11d26e76
Peter Ly <peter.ly1@hotmail.com>
2023-01-26 18:21:53 +0000

    Added another test

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -37,6 +37,21 @@ describe('getProduct', () => {
     const result = lib.getProduct(1);
     expect(result).toEqual({ id: 1, price: 10 });
     expect(result).toMatchObject({ id: 1, price: 10 });
-    expect(result).toHaveProperty( 'id', '1');
+    expect(result).toHaveProperty('id', 1);
+  });
+});
+
+describe('registerUser', () => {
+  it('should throw if username is falsy', () => {
+    const args = [null, undefined, NaN, '', 0, false];
+    args.forEach(a => {
+      expect(() => { lib.registerUser(null) }).toThrow();
+    });
+  });
+
+  it('should return a user object if valid username is passed', () => {
+    const result = lib.registerUser('pete');
+    expect(result).toMatchObject({ username: 'pete'});
+    expect(result.id).toBeGreaterThan(0);
   });
 });
